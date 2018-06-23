dadbce47be7988d973a448bfd0374b65ab2bdc14
chen-ray <yingzi.keren@gmail.com>
2018-06-23 16:24:19 +0000

    topic index page

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -6,4 +6,18 @@ class Topic extends Model
 {
     protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count',
         'last_reply_user_id', 'order', 'excerpt', 'slug'];
+
+    /**
+     * 一对一关联
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
