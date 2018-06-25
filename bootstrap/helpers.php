8f00edf63395b33a0a57f106b926205752b3b6b2
chen-ray <yingzi.keren@gmail.com>
2018-06-25 06:07:32 +0000

    admin can manage content

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -10,3 +10,35 @@ function make_excerpt($value, $length = 200)
     $excerpt    = trim(preg_replace('/\r\n|\r|\n+/', ' ', strip_tags($value)));
     return str_limit($excerpt, $length);
 }
+
+function model_admin_link($title, $model)
+{
+    return model_link($title, $model, 'admin');
+}
+
+function model_link($title, $model, $prefix = '')
+{
+    $model_name = model_plural_name($model);
+
+    //
+    $prefix = $prefix ? "/$prefix" : '/';
+
+    $url    = config('app.url') . $prefix . $model_name . '/' . $model->id;
+
+    return '<a href="' . $url . '" target="_blank"' . $title . '</a>';
+}
+
+function model_plural_name($model)
+{
+    // 从实体中获取完整类名， 例如 ： App\Models\User
+    $full_class_name    = get_class($model);
+
+    // 获取基础类名， 例如 传参 App\Models\User 会得到 User
+    $class_name         = class_basename($full_class_name);
+
+    // 蛇形命名， 例如， 传参 'User' 会得到 'user', 'FooBar' 会得到 foo_bar
+    $snake_case_name    = snake_case($class_name);
+
+    // 获取子串的复数形式， 例如 user = users
+    return str_plural($snake_case_name);
+}
